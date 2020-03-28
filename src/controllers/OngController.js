8c7d4528a3cc6559a8671a4d74ff4272e2c6f358
daviaugustoms <daviaugusto.developer@gmail.com>
2020-03-27 23:49:44 -0300

    alt constroll ong

---
+++ src/controllers/OngController.js
@@ -0,0 +1,27 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+
+module.exports = {
+    // Listagem de ongs
+    async index (request, response) {
+        const ongs = await connection('ongs').select('*');
+    
+        return response.json(ongs);
+    },
+    //Cadastro de ongs
+    async create(request, response) {
+        const { name, email, whatsapp, city, uf } = request.body;
+
+        const id = crypto.randomBytes(4).toString('HEX');
+
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        });
+    }
+}
