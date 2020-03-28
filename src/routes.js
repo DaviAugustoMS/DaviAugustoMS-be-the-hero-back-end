aeab56e840a2a778932fc05a5f37cc779c47d60a
daviaugustoms <daviaugusto.developer@gmail.com>
2020-03-28 14:30:00 -0300

    backend aplicação

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,21 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentsController = require('./controllers/IncidentsController');
+const ProfileController = require('./controllers/ProfileController.js');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
+
+// Login
+routes.post('/session', SessionController.create);
 
 // Listagem de ongs
 routes.get('/ongs', OngController.index);
 // Cadastro de ongs
 routes.post('/ongs', OngController.create);
+
+// Profile
+routes.get('/profile', ProfileController.index);
 
 // Cadastro de Incidets
 routes.post('/incidents', IncidentsController.create);
